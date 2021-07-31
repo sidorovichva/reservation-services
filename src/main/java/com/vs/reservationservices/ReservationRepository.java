9bc594c2fc33baf882e04936f6dcb4447eb9d5c2
sidorovichva <sidorovichva@gmail.com>
2021-07-31 19:42:07 +0300

    @EnableDiscoveryClient

--- src/main/java/com/vs/reservationservices/ReservationRepository.java
+++ src/main/java/com/vs/reservationservices/ReservationRepository.java
@@ -1,6 +1,8 @@
 package com.vs.reservationservices;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+//@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
